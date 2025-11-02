1d3013ae7e65ef0f6ae048e688e4527c988ad14f
arpitmeher <arpitmeher47@gmail.com>
2025-11-02 22:45:48 +0530

    add authentication

---
+++ auth.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from models import db, User
+from forms import RegisterForm, LoginForm
+
+auth_bp = Blueprint('auth', __name__)
+
+@auth_bp.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data,
+                    password=generate_password_hash(form.password.data))
+        db.session.add(user)
+        db.session.commit()
+        flash('Account created')
+        return redirect(url_for('auth.login'))
+    return render_template('register.html', form=form)
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Invalid credentials')
+    return render_template('login.html', form=form)
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
